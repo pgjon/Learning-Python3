8167ec81f1037f56b16c027f5ff351d6eddccb49
pgjon <jones.rgv@gmail.com>
2023-02-16 20:21:47 -0300

    Commit inicial

---
+++ Aula-001/Aula-001.py
@@ -0,0 +1,13 @@
+# isso é um comentário
+
+# exemplos de utilizalçao da função 'print'
+
+print('Olá, mundo!')
+print('meu nomé é Jones!')
+print('Començando em python.')
+
+# exemplo de uma operação matemática
+print(11 + 5)
+
+# concatenação
+print('11' + '5')
