b575f96b814a58c1193ece864df9242f7a072747
pgjon <jones.rgv@gmail.com>
2023-02-16 21:38:18 -0300

    Aula 2 concluida

---
+++ Aula-002/Aula-002.py
@@ -0,0 +1,18 @@
+# Variáveis
+
+nome = 'Jones'
+idade = 31
+peso = 102.3
+
+# saida
+print(nome, idade, peso)
+
+
+# Entrada de dados
+
+Nome = input('Qual seu nome? ')
+Idade = input('Qual sua idade? ')
+Peso = input('Qual seu peso? ')
+
+# saida
+print(Nome, Idade, Peso)
---
+++ Aula-002/exer-03.py
@@ -0,0 +1,6 @@
+n1 = int(input('Primeiro número? '))
+n2 = int(input('Segundo número? '))
+
+soma = n1 + n2
+
+print('A soma é = ', soma)
