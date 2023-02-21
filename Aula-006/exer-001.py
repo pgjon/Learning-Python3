a00b0d13fb3c82bd34946279e0c1b129b85d49fb
pgjon <jones.rgv@gmail.com>
2023-02-21 20:27:23 -0300

    Aula 006

--- Aula-006/exer-001.py
+++ Aula-006/exer-001.py
@@ -3,4 +3,4 @@ n2 = int(input('Digite outro número: '))
 
 soma = n1 + n2
 
-print('A soma entre {} e {} é = {}.'.format(n1, n2, soma))
+print(f'A soma entre {n1} e {n2} vale {soma}')
---
+++ Aula-006/exer-002.py
@@ -0,0 +1,7 @@
+algo = input('Digite algo: ')
+
+print('É alfabético? ', algo.isalpha())
+print('É númerico? ', algo.isnumeric())
+print('É tudo minúsculo? ', algo.islower())
+print('É tudo maiúsculo? ', algo.isupper())
+print('É Alfa-numérico?', algo.isalnum())
