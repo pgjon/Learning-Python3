954e7907673f823ac9eb1c78e778e81727af0998
pgjon <jones.rgv@gmail.com>
2023-02-27 22:49:27 -0300

    Correção

--- Exer-001-ao-100/Exer-018/exer-018.py
+++ Exer-001-ao-100/Exer-018/exer-018.py
@@ -1,14 +1,14 @@
-''' Faça um programa que leia um ângulo qualquer
-    e mostre na tela o valor do seno, cosseno
-    e tangente desse ângulo.    '''
 
-from math import cos, sin, tan
+from math import cos, radians, sin, tan
 
 print('Programa para descobrir Seno, Cosseno'
       ' e Tangente de um ângulo (x)')
 
 x = float(input('Informe o valor do ângulo: '))
+seno = sin(radians(x))
+cosseno = cos(radians(x))
+tangente = tan(radians(x))
 
-print(f'Seno do ângulo {x} é {sin(x) :.2f}')
-print(f'Cosseno do ângulo {x} é {cos(x) :.2f}')
-print(f'Tangente do ângulo {x} é {tan(x) :.2f}')
+print(f'Seno do ângulo {x}º é {seno :.2f}')
+print(f'Cosseno do ângulo {x}º é {cosseno :.2f}')
+print(f'Tangente do ângulo {x}º é {tangente :.2f}')
