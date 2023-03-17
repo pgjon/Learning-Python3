82897c219a6a1639ca9ec07926ba2ce6fcffc4fb
pgjon <jones.rgv@gmail.com>
2023-03-17 18:38:48 -0300

    Exercícios 001 a +100

---
+++ Exer-001-ao-100/Exer-026/exer-026-pt2.py
@@ -0,0 +1,10 @@
+from unidecode import unidecode  # bibliteca para remover os acentos
+
+frase = str(input('Digite uma frase: ')).lower().strip()
+letra = str(input('Qual letra deseja procurar na frase? ')).lower().strip()
+frase = unidecode(frase)
+
+print(f'\nA letra {letra} aparece', frase.count('a'), 'vez(es) na frase.')
+print(f'A primeira letra {letra} apareceu na posição', frase.find(letra) + 1)
+print(f'A última letra {letra} apareceu na posição',
+      frase.rfind(letra) + 1 - frase.count(' '))
